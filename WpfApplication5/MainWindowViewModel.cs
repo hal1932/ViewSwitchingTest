d4d6bceb4cbd860dc14c7662659af8fad4ee4b34
hal1932 <yu.arai.19@gmail.com>
2015-02-08 15:21:32 +0900

    initial commit

---
+++ WpfApplication5/MainWindowViewModel.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
+
+namespace WpfApplication5
+{
+    class ViewModelBase : BindableBase
+    {
+        #region Label変更通知プロパティ
+        private string _Label;
+
+        public string Label
+        {
+            get
+            { return _Label; }
+            set
+            { 
+                if (_Label == value)
+                    return;
+                _Label = value;
+                OnPropertyChanged("Label");
+            }
+        }
+        #endregion
+
+    }
+
+    class ViewModelA : ViewModelBase
+    {
+        public ViewModelA()
+        {
+            Label = "AAAAA";
+        }
+    }
+
+    class ViewModelB : ViewModelBase
+    {
+        public ViewModelB()
+        {
+            Label = "BBBBB";
+        }
+    }
+
+    class MainWindowViewModel : BindableBase
+    {
+        #region ContentViewModel変更通知プロパティ
+        private object _ContentViewModel;
+
+        public object ContentViewModel
+        {
+            get
+            { return _ContentViewModel; }
+            set
+            { 
+                if (_ContentViewModel == value)
+                    return;
+                _ContentViewModel = value;
+                OnPropertyChanged("ContentViewModel");
+            }
+        }
+        #endregion
+
+        #region SwitchContentViewModelCommand
+        private DelegateCommand<object> _SwitchContentViewModelCommand;
+
+        public DelegateCommand<object> SwitchContentViewModelCommand
+        {
+            get
+            {
+                if (_SwitchContentViewModelCommand == null)
+                {
+                    _SwitchContentViewModelCommand = new DelegateCommand<object>(SwitchContentViewModel);
+                }
+                return _SwitchContentViewModelCommand;
+            }
+        }
+
+        public void SwitchContentViewModel(object parameter)
+        {
+            if (ContentViewModel is ViewModelA) ContentViewModel = new ViewModelB();
+            else ContentViewModel = new ViewModelA();
+        }
+        #endregion
+
+
+        public MainWindowViewModel()
+        {
+            ContentViewModel = new ViewModelA();
+        }
+    }
+}
